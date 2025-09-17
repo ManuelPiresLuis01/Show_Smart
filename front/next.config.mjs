7c7bf8e563fc24b0e6ec064e392f6b88230a1d36
manuelpiresluis <manuelpires@gmail.com>
2025-09-17 18:04:28 +0100

    refactor: remove experimental features and enable CSS Modules in next.config.js; add react-is dependency

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -12,9 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Enable CSS Modules
-  cssModules: true,
-  // i18n configuration will be handled by next-i18next
 }
 
 export default nextConfig
